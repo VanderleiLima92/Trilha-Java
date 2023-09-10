53aa8d39827c760599acde470f27b30f5c9cdbc1
Vanderlei Lima <105977496+VanderleiLima92@users.noreply.github.com>
2023-09-10 16:19:51 -0300

    adicionado pasta de exercícios da faculdade

---
+++ Java_Basico/src/primeiras_aulas/exerciciosfaculdade/exerc01.java
@@ -0,0 +1,32 @@
+/*1. Escreva um programa que faz a leitura de um número inteiro e um número real. Caso o número inteiro seja menor
+do que o número real, o programa deve imprimir uma mensagem dizendo isso ao usuário. Caso contrário, o programa
+somente termina. */
+
+package primeiras_aulas.exerciciosfaculdade;
+
+import java.util.Scanner;
+
+public class exerc01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numInteiro;
+        double numReal;
+
+        System.out.println("Digite um número inteiro");
+        numInteiro = sc.nextInt();
+
+        System.out.println("Digite um número real");
+        numReal = sc.nextDouble();
+
+        if(numInteiro < numReal){
+            System.out.format("O número inteiro: %d é menor que o número real: %d \n", numInteiro, numReal);
+        }
+
+        else{
+            System.out.format("O número real: %d é menor que o número inteiro: %d \n", numReal, numInteiro );
+        }
+
+        sc.close();
+    }
+}
